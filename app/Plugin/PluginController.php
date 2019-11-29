8526bc20232f34b47d22b3dd0ddcecc405b5b5ce
kq <kq@qq.com>
2019-11-29 21:33:31 +0800

    增加插件设置页面设置SEO标题和关键词等

--- app/Plugin/PluginController.php
+++ app/Plugin/PluginController.php
@@ -10,6 +10,19 @@ class PluginController extends BaseController
     public $module = 'Plugin';//模块名字
     public $bladePrefix = '';
     public $controllerReplace='App\\Plugin\\';
+    public $seos;
+    public function setSeo($title, $keyword, $desc, $other = [])
+    {
 
+        $data = [
+            'title' => $title,
+            'keyword' => $keyword,
+            'description' => $desc
+        ];
+
+        $this->seos = $data;
+        view()->share($data);
+        return $data;
+    }
 
 }
