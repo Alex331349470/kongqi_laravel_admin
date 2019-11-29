16beabbcbcc6de28e9d9ade419c6729636a8ae95
kq <kq@qq.com>
2019-11-29 11:21:17 +0800

    修复角色编辑缺少JS特效

--- resources/views/admin/adminrole/edit.blade.php
+++ resources/views/admin/adminrole/edit.blade.php
@@ -46,10 +46,49 @@
 @endsection
 @section('foot_js')
     <script>
-        layui.use(['index', 'uploader'], function () {
+        layui.use(['index', 'uploader','form'], function () {
             var uploader = layui.uploader;
+            var form=layui.form;
             uploader.one("#thumbUpload");
-
+            form.on('checkbox', function (data) {
+                var check = data.elem.checked;//是否选中
+                var checkId = data.elem.id;//当前操作的选项框
+                if (check) {
+                    //选中
+                    var ids = checkId.split("-");
+                    if (ids.length == 3) {
+                        //第三极菜单
+                        //第三极菜单选中,则他的上级选中
+                        $("#" + (ids[0] + '-' + ids[1])).prop("checked", true);
+                        $("#" + (ids[0])).prop("checked", true);
+                    } else if (ids.length == 2) {
+                        //第二季菜单
+                        $("#" + (ids[0])).prop("checked", true);
+                        $("input[id*=" + ids[0] + '-' + ids[1] + "]").each(function (i, ele) {
+                            $(ele).prop("checked", true);
+                        });
+                    } else {
+                        //第一季菜单不需要做处理
+                        $("input[id*=" + ids[0] + "-]").each(function (i, ele) {
+                            $(ele).prop("checked", true);
+                        });
+                    }
+                } else {
+                    //取消选中
+                    var ids = checkId.split("-");
+                    if (ids.length == 2) {
+                        //第二极菜单
+                        $("input[id*=" + ids[0] + '-' + ids[1] + "]").each(function (i, ele) {
+                            $(ele).prop("checked", false);
+                        });
+                    } else if (ids.length == 1) {
+                        $("input[id*=" + ids[0] + "-]").each(function (i, ele) {
+                            $(ele).prop("checked", false);
+                        });
+                    }
+                }
+                form.render();
+            });
         })
     </script>
 
